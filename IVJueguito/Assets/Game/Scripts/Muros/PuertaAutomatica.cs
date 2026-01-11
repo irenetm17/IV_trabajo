08fe707978826231642d10ccca073ad41daf9370
EvilDevil23 <155478766+EvilDevil23@users.noreply.github.com>
2026-01-11 18:01:40 +0100

    puertas

---
+++ IVJueguito/Assets/Game/Scripts/Muros/PuertaAutomatica.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+public class PuertaAutomatica : MonoBehaviour, IObserver
+{
+
+    private Animator animator;
+
+    void Start()
+    {
+        EventManager.instance.Subscribir(eventType.DoorOpened, this);
+        animator = GetComponent<Animator>();
+    }
+
+    public void OnEvent(IEvent evento)
+    {
+        if (evento.Tipo == eventType.DoorOpened)
+        {
+            DoorOpenedEvent event4 = (DoorOpenedEvent)evento; //desempaqueta
+            if (event4.Target != this) return;
+            if (event4.Abrir)
+            {
+                AbrirPuerta();
+            }
+            else
+            {
+                CerrarPuerta();
+            }
+        }
+
+    }
+    void OnDestroy()
+    {
+        if (EventManager.instance != null)
+        {
+            EventManager.instance.Desuscribir(eventType.DoorOpened, this);
+        }
+    }
+
+    public void AbrirPuerta()
+    {
+        animator.SetBool("AbrirPuerta", true);
+    }
+
+    public void CerrarPuerta()
+    {
+        animator.SetBool("AbrirPuerta", false);
+    }
+}
